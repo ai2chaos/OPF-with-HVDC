54ddc58d919f364c76b7e84b07bdd0dd95227b03
powen.l@outlook.com <powen.l>
2015-04-10 13:38:23 +0800

    rename DATA.h -> dataStrruct.h

--- OPF of LoopNetwork with HVDC/testDemo/dataStruct.h
+++ OPF of LoopNetwork with HVDC/testDemo/dataStruct.h
@@ -1,8 +1,8 @@
 /* This file defines the struct of load flow data and 
    contains the variablze declarations and networks data */
 
-#ifndef _DATA_H_
-#define _DATA_H_
+#ifndef _DATASTRUCT_H_
+#define _DATASTRUCT_H_
 
 /*
  If there is a negative between i and j, it tells us
@@ -14,10 +14,30 @@ typedef struct 	Branch_Type
 	int i, j;	// i is headend node, j is end node
 	char name_i[10], name_j[10];	// name of headend&end node
 	double R, X, Y0, K;	
-}Line;
+}Branch;
 
-typedef struct Branch_Typr
+typedef struct Bus_type
 {
-	int 
-};
+	int seqNum, 
+}Bus;
+
+typedef struct Generator_Type
+{
+	double P, Q;	//P, Q is positive.
+	int i;
+	double V;
+}Generator;
+
+typedef struct Load_type
+{
+	double P, Q;	//P, Q is negative.
+	int i;
+	double V;
+}Load;
+
+typedef struct PVNode_Type
+{
+	double V;	//
+	int i;
+}PVNode;
 #endif
