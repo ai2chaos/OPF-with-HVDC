ef932ab2823094eef58c4ea1bff9be070d7a2bae
powen.l@outlook.com <powen.l>
2015-05-13 22:02:59 +0800

    测试矩阵

--- OPF of LoopNetwork with HVDC/testDemo/testMat.c
+++ OPF of LoopNetwork with HVDC/testDemo/testMat.c
@@ -6,25 +6,19 @@
 int main (void)
 {
 	//稀疏矩阵元素
-	int a11 = 1;
-	int a12 = 2;
-	int a13 = 3;
-	int a14 = 4;
-	int a21 = 5;
-	int a22 = 6;
-	int a23 = 7;
-	int a24 = 8;
-	int a33 = 5;
-	int a42 = -2;
-	int a44 = 6;
-
-	Mat A;	//指向稀疏矩阵A的指针，A的值为稀疏矩阵的地址
-	InitMat (&A);
-	addElement (a11, 1, 1, &A);
-	addElement (a12, 1, 2, &A);
-	addElement (a13, 1, 3, &A);
-	addElement (a21, 2, 1, &A);
-	showMat (&A);
+	int A[5][5];
+	int i, j, k;
+	Mat matA;
+	InitMat (&matA);
+	for ( i = 4, k = 1; i > 0; i-- )
+	{
+		for ( j = 4; j > 0; j--, k++ )
+		{
+			A[i][j] = k;
+			addElement (A[i][j], i, j, &matA);
+		}
+	}
+	showMat (&matA);
 	getchar ();
 	getchar ();
 	return 0;
